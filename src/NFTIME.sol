9436b94361db8b54c4c23f6c852f5988a54b455f
Olivier <olivier.winkler@icloud.com>
2023-03-23 19:33:14 +0100

    feat(comments): add comments / docu

--- src/NFTIME.sol
+++ src/NFTIME.sol
@@ -13,6 +13,38 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 import "./utils/DateTime.sol";
 import "./utils/Renderer.sol";
 
+/**                                        
+    c00l         ,k0d.  c0000000000000000d.  c000000000000000d.
+    oMMO:;'      :XM0'  dMMXkxkxxxxxxxxxkl.  ;kkxxxONMWKxxxxxl.
+    dMMMWM0'     :XM0'  dMMx.                      .OMNc       
+    dMM0l:codl.  :XM0'  dMMd.                      .OMN:       
+    dMMx. .xXKc..cNM0'  dMMx..........             .OMN:       
+    dMMd   ..'o00XWM0'  dMMNK000000000l            .OMN:       
+    dMMd.     ;xx0WM0'  dMMXkxkxxxxxxk:            .OMN:       
+    dMMd.        :XM0'  dMMx.                      .OMN:       
+    dMMd         :XM0'  dMMd                       .OMN:       
+    dMMd         :XM0'  dMMd                       .OMN:       
+    dMMd         :XM0'  dMMd                       .OMN:       
+    ;xx:         'dkl.  ;xx:                       .lkd'                                                      
+                                                               
+     ...............     ..             .     ...............  
+    c000000000000000d.  c00c          ,k0d.  c000000000000000d.
+    ;xxxkxONMWKxkxxkl.  dMMO:,'    .;,dNM0'  dMMXkxxxxxxkkxxkl.
+          .OMNc         oMMMWM0'   dWMMMM0'  dMMx.             
+          .OMN:         dMM0l:codoolccxNM0'  dMMd              
+          .OMN:         dMMd. .xNXK;  :NM0'  dMMx.........     
+          .OMN:         dMMx.  ....   :NM0'  dMMNK00000000c    
+          .OMN:         dMMx.         :NM0'  dMMXkxxxxxxxx:    
+          .OMN:         dMMx.         :NM0'  dMMx.             
+          .OMN:         dMMx.         :NM0'  dMMd.             
+     .....,0MNl.....    dMMd          :XM0'  dMMx............  
+    c00000KWMMX00000d.  dMMd          :XM0'  dMMNK00000000000d.
+    ;xxxxxxxxxxkkxxkl.  ;xk:          'dkl.  ;xxxxxxxxxxxxxxkl.
+
+@title  NFTIME
+@author NFTXYZ.ch (Olivier Winkler)
+@notice MINT YOUR MINUTE
+*/
 contract NFTIME is
     Ownable,
     AccessControl,
@@ -28,23 +60,27 @@ contract NFTIME is
 
     Renderer public renderer;
 
+    /// @notice Minting events
     event Mint(address indexed _address, uint256 _tokenId);
     event RarityMint(address indexed _address, uint256 _tokenId);
 
-    // 01.JAN 2030 12:00 -> true / false
+    /// @notice 01.JAN 2030 12:00 -> true / false
     mapping(string => bool) public mintedTimes;
 
-    // 1 -> 01.JAN 2030 12:00
+    /// @notice 1 -> 01.JAN 2030 12:00
     mapping(uint256 => string) public tokenIdToTime;
 
-    // 1 -> [year:'2030',month:'JAN',day:'01',hour:'11',minute:'00']
+    /// @notice 1 -> [year:'2030',month:'JAN',day:'01',hour:'11',minute:'00']
     mapping(uint256 => Date) public tokenIdToTimeStruct;
 
+    /// @dev Initialize NFTIME Contract, grant DEFAULT_ADMIN_ROLE & set {Renderer}.
     constructor(address _renderer, address _multisig) ERC721("NFTIME", "TIME") {
         _grantRole(DEFAULT_ADMIN_ROLE, _multisig);
         renderer = Renderer(_renderer);
     }
 
+    /// @dev Mint Regular NFTIME
+    /// @param _time Timestamp for minted NFTIME
     function mint(
         uint256 _time
     ) public payable whenNotPaused returns (uint256) {
@@ -72,6 +108,8 @@ contract NFTIME is
         return newItemId;
     }
 
+    /// @dev Mint Rarity NFTIME
+    /// @param _tokenUri TokenURI to special rarity NFTIME
     function rarityMint(
         string memory _tokenUri
     ) public onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
@@ -87,6 +125,8 @@ contract NFTIME is
         return newItemId;
     }
 
+    /// @dev Render the JSON Metadata for a given NFTIME.
+    /// @param _tokenId The id of the token to render.
     function tokenURI(
         uint256 _tokenId
     )
@@ -126,14 +166,18 @@ contract NFTIME is
             );
     }
 
+    /// @dev IPFS Link to Opensea Collection Metadata.
     function contractURI() public pure returns (string memory) {
         return "ipfs://QmU5vDC1JkEASspnn3zF5WKRraXhtmrKPvWQL2Uwh6X1Wb";
     }
 
+    /// @dev Base URI for Metadata.
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
     }
 
+    /// @dev Concat all the date attributes.
+    /// @param date The used datestruct {Datetime}.
     function getAttributes(
         Date memory date
     ) internal pure returns (string memory) {
@@ -153,6 +197,9 @@ contract NFTIME is
             );
     }
 
+    /// @dev Concat metadata attribute.
+    /// @param label Label of the attribute.
+    /// @param attribute Value of the attribute.
     function concatAttribute(
         string memory label,
         string memory attribute
@@ -168,6 +215,8 @@ contract NFTIME is
             );
     }
 
+    /// @dev Encodes svg into base64.
+    /// @param _svg SVG code.
     function svgToImageURI(
         string memory _svg
     ) internal pure returns (string memory) {
@@ -178,26 +227,33 @@ contract NFTIME is
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
+    /// @dev Set a new Renderer Contract.
+    /// @param _renderer New Renderer Address.
     function setRenderer(
         Renderer _renderer
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         renderer = _renderer;
     }
 
+    /// @dev Update DEFAULT_ADMIN_ROLE.
+    /// @param _multisig New multisig address.
     function setDefaultAdminRole(
         address _multisig
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(DEFAULT_ADMIN_ROLE, _multisig);
     }
 
+    /// @dev Stop Minting
     function pauseTransactions() external onlyRole(DEFAULT_ADMIN_ROLE) {
         _pause();
     }
 
+    /// @dev Resume Minting
     function resumeTransactions() external onlyRole(DEFAULT_ADMIN_ROLE) {
         _unpause();
     }
 
+    /// @dev Withdraw collected Funds
     function withdraw() public payable onlyRole(DEFAULT_ADMIN_ROLE) {
         (bool success, ) = payable(msg.sender).call{
             value: address(this).balance
