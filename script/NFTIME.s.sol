5849f06889afc8d03c7406156143354406024bc6
Olivier <olivier.winkler@icloud.com>
2023-05-13 19:50:55 +0200

    feat(contracts): add example ipfs link in deploy script

--- script/NFTIME.s.sol
+++ script/NFTIME.s.sol
@@ -13,12 +13,12 @@ contract CounterScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        NFTIME nftime = new NFTIME(
-            0x94B2ceA71F9bA7A6e55c40bE320033D1151145B6,
-            "https://nftime-owieth.vercel.app/clock"
-        );
+        NFTIME nftime = new NFTIME(0x94B2ceA71F9bA7A6e55c40bE320033D1151145B6);
 
-        nftime.mint{value: 0.01 ether}(1893495600);
+        nftime.mint{value: 0.01 ether}(
+            1893495600,
+            "ipfs://QmW5FZv7nrpBMqF2HTPt2CGcea87FdcEPbpo9dwZpUCg1b"
+        );
 
         vm.stopBroadcast();
     }
